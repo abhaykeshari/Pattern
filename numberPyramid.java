662df2074f58f639dab126d487df3e3ad9dfc03f
Abhay kesharwani <shiyalal11@gmail.com>
2026-06-03 17:31:01 +0530

    Refactor numberPyramid class and update logic

--- numberPyramid.java
+++ numberPyramid.java
@@ -1,15 +1,13 @@
-public class numberPyramid {
+public class numberpyramid {
     public static void main(String[] args){
-        int n =4 ;
-        for(int i = 1 ; i<=n ; i++){
-            for(int j = 1 ; j <= n -i ; j++){
+        int n = 5 ;
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n-i ; j++){
                 System.out.print(" ");
             }
-            for(int k = 1 ; k <=i; k++){
-                System.out.print(k);
-            }
-            for(int l = i     ; l>=2;l--){
-                System.out.print(l-1);
+            for(int j = 1 ; j <= 2*i-1;j++){
+                System.out.print(i);
+                //System.out.print(" ");
             }
             System.out.println();
         }
